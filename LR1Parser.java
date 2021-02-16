a39e321adf67f92c8c491ded101a55bb4b674309
LittleB30 <50643241+LittleB30@users.noreply.github.com>
2021-02-16 11:54:57 -0500

    class skeletons complete

--- LR1Parser.java
+++ LR1Parser.java
@@ -1,10 +1,129 @@
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * A class defining an LR(1), shift-reduce, parser containing a parse table, a token queue, and a parse stack.
+ * @author Alex Smith (alsmi14)
+ */
 public class LR1Parser {
-    
-    private class QueueNode {
+    private Table parseTable;
+    private Queue<QueueNode> tokenQueue;
+    private Stack<StackNode> parseStack;
+
+    /**************
+      Constructors
+     **************/
+
+    /**
+     * 
+     * @param t
+     */
+    public LR1Parser(String[] t) { //TODO implement and comment
+
+    }
+
+    /****************
+      Public Methods
+     ****************/
+
+    /**
+     * 
+     * @param exp
+     */
+    public void evaluate(String exp) { //TODO implement and comment
 
     }
     
+    private class QueueNode {
+        String token;
+        boolean isNum;
+        int value;
+
+        /**************
+          Constructors
+         **************/
+
+        /**
+         * 
+         * @param tok
+         */
+        public QueueNode(String tok) { //TODO implement and comment
+
+        }
+
+        /****************
+          Public Methods
+         ****************/
+
+        /**
+         * 
+         * @return
+         */
+        public String getToken() { //TODO implement and comment
+            
+        }
+
+        /**
+         * 
+         * @return
+         */
+        public boolean getIsNum() { //TODO implement and comment
+            
+        }
+
+        /**
+         * 
+         * @return
+         */
+        public int getValue() { //TODO implement and comment
+
+        }
+
+        /**
+         * 
+         */
+        public String toString() { //TODO implement and comment
+
+        }
+    }
+    
     private class StackNode extends QueueNode {
+        private int curState;
+
+        /**************
+          Constructors
+         **************/
+
+        /**
+         * 
+         * @param tok
+         * @param state
+         */
+        public StackNode(String tok, int state) { //TODO implement and comment
+            super(tok);
+        }
+
+        /****************
+          Public Methods
+         ****************/
+
+        public int getCurState() { //TODO implement and comment
+
+        }
+
+        public String toString() { //TODO implement and comment
+
+        }
+    }
+
+    /*****************
+      Private Methods
+     *****************/
+
+    /**
+     * 
+     */
+    private void printIteration() { //TODO implement and comment
 
     }
 }
--- Table.java
+++ Table.java
@@ -1,9 +1,180 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * A simple table object with rows and columns implmented with a map.
+ * @author Alex Smith (alsmi14)
+ */
 public class Table {
+    private HashMap<String[],Entry> table;
 
+    /**************
+      Constructors
+     **************/
+
+    /**
+     * 
+     * @param t
+     */
+    public Table(String[] t) { //TODO implement and comment
+
+    }
+
+    /****************
+      Public Methods
+     ****************/
+
+    /**
+     * 
+     * @param row
+     * @param col
+     * @return
+     */
+    public Entry getEntry(String row, String col) { //TODO implement and comment
+
+    }
+
+    /**
+     * 
+     */
     public class Entry {
+        private boolean isShift;
+        private ArrayList<CFRule> rules;
+        private int shiftTo;
 
-        public class CFRule {
+        /**************
+          Constructors
+         **************/
+
+        /**
+         * 
+         * @param shift
+         */
+        public Entry(int shift) { //TODO implement and comment
 
         }
+        
+        /**
+         * 
+         * @param rules
+         */
+        public Entry(String rules) { //TODO implement and comment
+            
+        }
+
+        /****************
+          Public Methods
+         ****************/
+
+        /**
+         * 
+         * @return
+         */
+        public boolean getIsShift() { //TODO implement and comment
+
+        }
+
+        /**
+         * 
+         * @return
+         */
+        public ArrayList<CFRule> getRules() { //TODO implement and comment
+
+        }
+
+        /**
+         * 
+         * @return
+         */
+        public int getShiftTo() { //TODO implement and comment
+
+        }
+
+        /**
+         * 
+         */
+        public String toString() { //TODO implement and comment
+            
+        }
+
+        /**
+         * 
+         */
+        public class CFRule {
+            String left;
+            String[] right;
+
+            /**************
+              Constructors
+             **************/
+
+            /**
+             * 
+             * @param l
+             * @param r
+             */
+            public CFRule(String l, String[] r) { //TODO implement and comment
+
+            }
+
+            /****************
+              Public Methods
+             ****************/
+
+            /**
+             * 
+             * @return
+             */
+            public String getLeft() { //TODO implement and comment
+
+            }
+
+            /**
+             * 
+             * @return
+             */
+            public String[] getRight() { //TODO implement and comment
+
+            }
+
+            /**
+             * 
+             */
+            public String toString() { //TODO implement and comment
+
+            }
+        }
+    }
+
+    /*****************
+      Private Methods
+     *****************/
+
+    /**
+     * 
+     * @param t
+     */
+    private void parseTable(String[] t) { //TODO implement and comment
+
+    }
+
+    /**
+     * 
+     * @param row
+     * @param col
+     * @param shift
+     */
+    private void addEntry(String row, String col, int shift) { //TODO implement and comment
+
+    }
+
+    /**
+     * 
+     * @param row
+     * @param col
+     * @param rules
+     */
+    private void addEntry(String row, String col, String rules) { //TODO implement and comment
+
     }
 }
